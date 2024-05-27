c50d342505d82d1885accafc20bb605920c159f5
Atik Rahad <atikrahad1@gmail.com>
2024-05-27 12:43:15 +0600

    adding mongodb datavbase uri

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import app from "./app";
-const port = 5000
+import mongoose from "mongoose";
+import config from "./app/config";
 
-app.listen(port, ()=>{
-    console.log("Server is running with", port)
-})
+const main = async () => {
+    try {
+        await mongoose.connect(config.database as string)
+
+        app.listen(config.port, () => {
+            console.log("Server is running with", config.port)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main()
