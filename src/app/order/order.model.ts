c4bdfb1ee570c5d2b7da6987c8562a0561931f3b
Atik Rahad <atikrahad1@gmail.com>
2024-05-27 14:38:30 +0600

    adding order model

---
+++ src/app/order/order.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import { TOrder } from "./order.interface";
+
+const orderSchema = new Schema<TOrder>({
+    email: {
+        type: String,
+        required: true
+    },
+    productId: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const order = model<TOrder>("order", orderSchema)
+export default order
